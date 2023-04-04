3fa1a9baf37233c25dbc0daedfa6d90cb1832402
simonlaraway <126140438+simonlaraway@users.noreply.github.com>
2023-04-04 12:35:52 -0600

    Delete aggregate.py

--- aggregate.py
+++
@@ -1,7 +0,0 @@
-import glob
-
-path = ["/Users/simonlaraway/Desktop/collectimdb/Cast/*", "/Users/simonlaraway/Desktop/collectimdb/Crew/*"] #replace with correct file path
-
-with open ("rockdata.csv", 'r') as f:
-    for line in f:
-        print(line)
