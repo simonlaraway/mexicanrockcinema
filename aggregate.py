c458695c68dfd0ae0c013df8f313624f9d9d18d2
simonlaraway <126140438+simonlaraway@users.noreply.github.com>
2023-04-03 22:22:19 -0600

    Update aggregate.py

--- aggregate.py
+++ aggregate.py
@@ -1,14 +1,6 @@
 import glob
 
-path = ["/Users/simonlaraway/Desktop/collectimdb/Cast/*", "/Users/simonlaraway/Desktop/collectimdb/Crew/*"]
-
-# /Users/simonlaraway/Desktop/collectimdb/imdb_cast_crew_scraped/5 de chocolate y 1 de fresa _castcrew
-
-# for item in path:
-#     for file in glob.glob(item):
-#         with open(file, 'r') as f:
-#             with open("aggregatedfiles.csv", 'a') as g:
-#                 g.write(f.read() + "\n\n" + "###" + "\n\n")
+path = ["/Users/simonlaraway/Desktop/collectimdb/Cast/*", "/Users/simonlaraway/Desktop/collectimdb/Crew/*"] #replace with correct file path
 
 with open ("rockdata.csv", 'r') as f:
     for line in f:
