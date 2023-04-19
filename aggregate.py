51e73144a35dddbdfb990d5d09a73e8cb65005f9
simonlaraway <126140438+simonlaraway@users.noreply.github.com>
2023-04-19 15:11:15 -0600

    Add files via upload

---
+++ aggregate.py
@@ -0,0 +1,11 @@
+import glob
+
+path = ["/Users/simonlaraway/Desktop/collectimdb/Cast/*", "/Users/simonlaraway/Desktop/collectimdb/Crew/*"]
+
+# /Users/simonlaraway/Desktop/collectimdb/imdb_cast_crew_scraped/5 de chocolate y 1 de fresa _castcrew
+
+for item in path:
+    for file in glob.glob(item):
+        with open(file, 'r') as f:
+            with open("aggregatedfiles.csv", 'a') as g:
+                g.write(f.read() + "\n\n" + "###" + "\n\n")
