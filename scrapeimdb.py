adb78d2fe1295189055854b5a5c8e6633567dd92
simonlaraway <126140438+simonlaraway@users.noreply.github.com>
2023-04-22 15:35:55 -0600

    Update scrapeimdb.py

--- scrapeimdb.py
+++ scrapeimdb.py
@@ -3,7 +3,8 @@ import csv
 
 """
 This code scrapes the html from IMDB cast and crew pages. 
-The csv it retrieves the URLS from should be formatted like rockdata.csv, with the link to the cast and crew pages for each respective film as the 6th item 
+
+The csv it retrieves the URLS from should be formatted like rockdata.csv, with the link to the imdb page for each respective film as the 6th item 
 on each line. Alternatively, you can manipulate the code beginning on line 28 to take a list of tuples or something with the name of the film and the link to
 the imdb cast+crew page.
 It will print the html for each film in a separate file. 
@@ -15,23 +16,23 @@ Before you run this code, make sure that you have a folder titled "imdb_cast_cre
 header={'user-agent': '[Your Name]', } # replace w your name and credentials
 
 
-def gethtml(title, imdb):
-    """
-    
-    """
-    html = requests.get(imdb, headers=header)
+def gethtml(title, imdbcorrect):
+    html = requests.get(imdbcorrect, headers=header)
     htmltext = html.text
-    with open(f"imdb_cast_crew_scraped/{title}_castcrew", 'w') as f: 
+    with open(f"{folder}/{title}_castcrew", 'w') as f: 
         f.write(title)
         f.write(htmltext)
-    print(html.encoding)
 
 with open("rockdata.csv", 'r') as f:
     csv_reader = csv.reader(f, delimiter=",")
+    global i
+    i = 0
     for row in csv_reader:
+        i += 1
+        # print(i)
         if "imdb" in row[6]:
             title = row[0]
             # print(title)
             imdb = row[6]
-            # print(imdb)
-            gethtml(title, imdb)
+            imdbcorrect = re.sub(r"\?ref.+", "fullcredits?ref_=tt_cl_sm", imdb)
+            gethtml(title, imdbcorrect)
